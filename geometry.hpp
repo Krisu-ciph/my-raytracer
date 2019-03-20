c4edd07b7202e1e210fc4675b155b713ece26706
Krisu-ciph <krisu-ciph@qq.com>
2019-03-20 22:13:38 +0800

    add geometry.hpp(not finished yet)

--- geometry.hpp
+++ geometry.hpp
@@ -1,65 +1,105 @@
-#ifndef geometry_h
-#define geometry_h
-
 #include <cmath>
+#include <algorithm>
 #include <iostream>
+#include <cstdlib>
 
-template<typename T>
-class Vec3 {
+#define CHECK(condition) \
+    if (!(consdition)) { cerr << "Check failed: " << #condition << endl; } \
+    exit(-1);
+
+namespace krs {
+
+inline bool isNaN(const T x) {
+    return std::isnan(x);
+}
+
+template <typename T>
+class Vector3 {
 public:
-    T x, y, z;
-    Vec3() : x(T(0)), y(T(0)), z(T(0)) {}
-    Vec3(T x) : x(x), y(x), z(x) {}
-    Vec3(T x, T y, T z) : x(x), y(y), z(z) {}
-    Vec3& normalize() {
-        T nor2 = length2();
-        if (nor2 > 0) {
-            T invNor = 1 / sqrt(nor2);
-            x *= invNor, y *= invNor, z *= invNor;
-        }
+    T operator[](int i) const {
+        CHECK(i >= 0 && i <= 2);
+        if (i == 0) return x;
+        if (i == 1) return y;
+        return z;
+    }
+    T &operator[](int i) {
+        CHECK(i >= 0 && i <= 2);
+        if (i == 0) return x;
+        if (i == 1) return y;
+        return z;
+    }
+    Vector3() { x = y = z = 0; }
+    Vector3(T x, T y, T z) : x(x), y(y), z(z) { CHECK(!HasNaNs()); }
+    bool HasNaNs() const { return isNaN(x) || isNaN(y) || isNaN(z); }
+    Vector3(const Vector3<T> &v) {
+        CHECK(!v.HasNaNs());
+        x = v.x;
+        y = v.y;
+        z = v.z;
+    }
+
+    Vector<T> &operator=(const Vector3<T> &v) {
+        CHECK(!v.HasNaNs());
+        x = v.x;
+        y = v.y;
+        z = v.z;
+    }
+
+    Vector3<T> operator+(const Vector3<T> &v) const {
+        CHECK(!v.HasNaNs());
+        return Vector3(x + v.x, y + v.y, z + v.z);
+    }
+    Vector3<T> &operator+=(const Vector3<T> &v) {
+        CHECK(!v.HasNaNs());
+        x += v.x;
+        y += v.y;
+        z += v.z;
         return *this;
     }
-    Vec3<T> operator * (const T &f) const { return Vec3<T>(x * f, y * f, z * f); }
-    Vec3<T> operator * (const Vec3<T> &v) const { return Vec3<T>(x * v.x, y * v.y, z * v.z); }
-    T dot(const Vec3<T> &v) const { return x * v.x + y * v.y + z * v.z; }
-    Vec3<T> operator - (const Vec3<T> &v) const { return Vec3<T>(x - v.x, y - v.y, z - v.z); }
-    Vec3<T> operator + (const Vec3<T> &v) const { return Vec3<T>(x + v.x, y + v.y, z + v.z); }
-    Vec3<T>& operator += (const Vec3<T> &v) { x += v.x, y += v.y, z += v.z; return *this; }
-    Vec3<T>& operator *= (const Vec3<T> &v) { x *= v.x, y *= v.y, z *= v.z; return *this; }
-    Vec3<T> operator - () const { return Vec3<T>(-x, -y, -z); }
-    T length2() const { return x * x + y * y + z * z; }
-    T length() const { return sqrt(length2()); }
-    friend std::ostream & operator << (std::ostream &os, const Vec3<T> &v) {
-        os << "[" << v.x << " " << v.y << " " << v.z << "]";
-        return os;
+    Vector3<T> operator-(const Vector3<T> &v) const {
+        CHECK(!v.HasNaNs());
+        return Vector3(x - v.x, y - v.y, z - v.z);
     }
-};
-
-typedef Vec3<float> Vec3f;
-
-class Sphere {
-public:
-    Vec3f center;
-    float radius, radius2;
-    Vec3f surfaceColor, emissionColor;
-    float transparency, reflection;
-    Sphere(const Vec3f &c, const float r, const Vec3f &sc, const float refl = 0, const float transp = 0, const Vec3f &ec = 0) :
-    center(c), radius(r), radius2(r * r), surfaceColor(sc), emissionColor(ec), transparency(transp), reflection(refl) {}
-    // compute a ray-sphere intersection using the geometry solution
-    // the theory of this computation is remained unknown
-    bool intersect(const Vec3f &rayorig, const Vec3f &raydir, float &t0, float &t1) const {
-        Vec3f l = center - rayorig;
-        float tca = l.dot(raydir);
-        if (tca < 0)
-            return false;
-        float d2 = l.length2() - tca * tca;
-        if (d2 > radius2)
-            return false;
-        float thc = sqrt(radius2 - d2);
-        t0 = tca - thc;
-        t1 = tca + thc;
-        return true;
+    Vector3<T> &operator-=(const Vector3<T> &v) {
+        CHECK(!v.HasNaNs());
+        x -= v.x;
+        y -= v.y;
+        z -= v.z;
+        return *this;
     }
-};
 
-#endif
+    bool operator==(const Vector3<T> &v) const {
+        return x == v.x && y == v.y && z == v.z;
+    }
+    bool operator!=(const Vector<T> &v) const {
+        return x != v.x && y != v.y && z != v.z;
+    }
+    template<typename U>
+    Vector3<T> operator*(U s) const {
+        CHECK(!isNaN(s));
+        return Vector3<T>(s * x, s * y, s * z);
+    }
+    template<typename U>
+    Vector3<T> &operator*=(U s) {
+        CHECK(!isNaN(s));
+        x *= s;
+        y *= s;
+        z *= s;
+        return *this;
+    }
+    template<typename U>
+    Vector3<T> operator*(U s) const {
+        CHECK(!isNaN(s));
+        return Vector3<T>(s / x, s  y, s * z);
+    }
+    template<typename U>
+    Vector3<T> &operator*=(U s) {
+        CHECK(!isNaN(s));
+        x *= s;
+        y *= s;
+        z *= s;
+        return *this;
+    }
+}
+
+}
