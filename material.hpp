063c011e55c131509616750e56be873c38bbce44
Krisu-ciph <krisu-ciph@qq.com>
2019-03-25 21:28:09 +0800

    finish refraction

--- material.hpp
+++ material.hpp
@@ -2,11 +2,17 @@
 #define material_hpp
 
 #include "hitable.hpp"
+#include <cmath>
 
 vec3 random_in_unit_sphere();
 vec3 reflect(const vec3 &v, const vec3 &n);
 bool refract(const vec3 &v, const vec3 &normal, float ni_over_nt,
             vec3 &refracted);
+float schlick(float cosine, float ref_idx) {
+    float r0 = (1-ref_idx) / (1+ref_idx);
+    r0 = r0 * r0;
+    return r0 + (1-r0)*std::pow((1-cosine), 5);
+}
 
 class material {
 public:
@@ -64,23 +70,35 @@ public:
         attenuation = vec3(1.0, 1.0, 1.0);
         vec3 refracted;
 
+        float reflect_prob;
+        float cosine;
+
         // Check whether light is leaving or getting into the material?
         if (dot(r_in.direction, rec.normal) > 0) {
             outward_normal = -rec.normal;
             ni_over_nt = ref_idx;
+            cosine = ref_idx * dot(r_in.direction, rec.normal) /
+                r_in.direction.length();
+
         } else {
             outward_normal = rec.normal;
             ni_over_nt = 1.0 / ref_idx;
+            cosine = -dot(r_in.direction, rec.normal) / r_in.direction.length();
         }
 
-        // Check if it's total internal reflection
         if (refract(r_in.direction, outward_normal, ni_over_nt, refracted)) {
-            scattered = ray(rec.p, refracted);
-            return true;
+            reflect_prob = schlick(cosine, ref_idx);
         } else {
             scattered = ray(rec.p, reflected);
-            return false;
+            reflect_prob = 1.0;
         }
+
+        if (drand48() < reflect_prob) {
+            scattered = ray(rec.p, reflected);
+        } else {
+            scattered = ray(rec.p, refracted);
+        }
+        return true;
     }
 
     float ref_idx;
